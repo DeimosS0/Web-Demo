be26be0df5f67226d9bec9bfd4f86dac7e584e16
Deimos <79209149+DeimosS0@users.noreply.github.com.>
2026-03-08 14:06:12 +0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://makineceviri.net',
-  base: '/',
-  trailingSlash: 'always', 
   output: 'static',
 });
